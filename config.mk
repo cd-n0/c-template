acf7cc2a16f483306d35fe2762c1bfc405fb081d
cdn0 <59338209+cd-n0@users.noreply.github.com>
2024-12-23 17:34:23 +0300

    Added config.mk and modified Makefile so that it includes it

---
+++ config.mk
@@ -0,0 +1,32 @@
+CC=gcc
+CFLAGS=-std=c89 -I$(INCDIR) -Wall -Wextra -Wpedantic
+LDLIBS=
+DEBUG_FLAGS = -ggdb3
+RELEASE_FLAGS = -O2
+
+# Default build is debug
+BUILD ?= debug
+
+ifeq ($(BUILD), debug)
+    CFLAGS += $(DEBUG_FLAGS)
+else ifeq ($(BUILD), release)
+    CFLAGS += $(RELEASE_FLAGS)
+else
+    $(error Invalid BUILD value)
+endif
+
+# Test source files
+TESTSRCS = $(wildcard tests/*.c)
+
+# Test executables
+TESTS = $(TESTSRCS:.c=)
+
+# Directories
+SRCDIR = src
+INCDIR = $(SRCDIR)/include
+OBJDIR = obj
+
+SRCS = $(wildcard $(SRCDIR)/*.c)
+OBJS = $(patsubst $(SRCDIR)/%.c,$(OBJDIR)/%.o,$(SRCS))
+
+TARGET=target.out
